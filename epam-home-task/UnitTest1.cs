5aa5dd9c7e96be82a3a82e056368f9ea47aca2e1
Camilo Fonseca <camilo_fonseca@epam.com>
2024-03-01 12:50:40 -0500

    Changed project and test class format.

--- epam-home-task/UnitTest1.cs
+++ epam-home-task/UnitTest1.cs
@@ -2,6 +2,8 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
 
 namespace epam_home_task
 {
@@ -10,52 +12,149 @@ namespace epam_home_task
         private IWebDriver driver;
 
         //Locators for ValidateCriteriaBasedPositionSearch
-        private readonly string acceptCookiesButtonLocator = "//button[contains(text(), 'Accept')]"; //Xpath
-        private readonly string languageTextBoxLocator = "new_form_job_search-keyword"; //Id
-        private string allLocationsOptionLocator = "li[title='{0}']"; //css
-        private readonly string locationDropdownButtonLocator = "span[role=presentation]"; //css
-        private readonly string dropDownMenuLocator = "ul[role] .os-content"; //css
-        private readonly string careersTopMenuLocator = "//a[@class='top-navigation__item-link js-op'][.='Careers']"; //Xpath
-        private readonly string remoteCheckboxLocator = "[name=remote]+label"; //css
-        private readonly string findButtonLocator = "//button[contains(text(), 'Find')]"; //Xpath
-        private readonly string viewAndApplyButtonLocator = "li.search-result__item:last-child .button-text"; //css
-        private readonly string viewMoreButtonLocator = "//a[.='View More']"; //Xpath
+        private readonly By acceptCookiesButtonLocator = By.CssSelector("div button#onetrust-accept-btn-handler"); //Xpath
+        private readonly By languageTextBoxLocator = By.Id("new_form_job_search-keyword"); //Id        
+        private readonly By locationDropdownButtonLocator = By.CssSelector("span[role=presentation]"); //css
+        private readonly By locationDropdownMenuLocator = By.CssSelector("ul[role] .os-content"); //css
+        private readonly By careersTopMenuLocator = By.XPath("//a[@class='top-navigation__item-link js-op'][.='Careers']"); //Xpath
+        private readonly By remoteCheckboxLocator = By.CssSelector("[name=remote]+label"); //css
+        private readonly By findButtonLocator = By.XPath("//button[contains(text(), 'Find')]"); //Xpath
+        private readonly By viewAndApplyButtonLocator = By.CssSelector("li.search-result__item:last-child .button-text"); //css
+        private readonly By selectedLocationLocator = By.CssSelector(".select2-selection__rendered"); //css
+        private string allLocationsOptionLocator = ".select2-results__option [title='{0}']"; //css
+
+
 
         //Locators for ValidateGlobalSearch
-        private readonly string searchIconLocator = "span.search-icon"; //css
-        private readonly string searchTextBoxLocator = "q"; //Name
-        private readonly string mainFindButtonLocator = "//span[contains(text(), 'Find')]"; //Xpath
-        private readonly string listElementLocator = ".search-results__title-link"; //css
-        private readonly string footerLocator = "footer.search-results__footer"; //css
+        private readonly By searchIconLocator = By.CssSelector("span.search-icon"); //css
+        private readonly By searchTextBoxLocator = By.Name("q"); //Name
+        private readonly By mainFindButtonLocator = By.XPath("//span[contains(text(), 'Find')]"); //Xpath
+        private readonly By listElementLocator = By.CssSelector(".search-results__title-link"); //css
+        private readonly By footerLocator = By.CssSelector("footer.search-results__footer"); //css
 
-        private string pageSource;
+        private string? pageSource;
 
-        public void AcceptCookies(WebDriverWait wait)
+        private IWebElement GetAcceptCookiesButton() { return driver.FindElement(acceptCookiesButtonLocator); }
+        private IWebElement GetLanguageTextBox() { return driver.FindElement(languageTextBoxLocator); }
+        private IWebElement GetLocationDropdownButton() { return driver.FindElement(locationDropdownButtonLocator); }
+        private IWebElement GetLocationDropdownMenu() { return driver.FindElement(locationDropdownMenuLocator); }
+        private IWebElement GetCareersTopMenu() { return driver.FindElement(careersTopMenuLocator); }
+        private IWebElement GetRemoteCheckbox() { return driver.FindElement(remoteCheckboxLocator); }
+        private IWebElement GetFindButton() { return driver.FindElement(findButtonLocator); }
+        private IWebElement GetViewAndApplyButton() { return driver.FindElement(viewAndApplyButtonLocator); }
+        private IWebElement GetSelectedLocation() { return driver.FindElement(selectedLocationLocator); }
+        private IWebElement GetAllLocationsOption(string option)
         {
-            var condition1 = wait.Until(d => d.FindElement(By.XPath(acceptCookiesButtonLocator)));
-            var condition2 = wait.Until(d => d.FindElement(By.XPath(acceptCookiesButtonLocator)).Enabled);
-            IWebElement acceptCookiesButton = driver.FindElement(By.XPath(acceptCookiesButtonLocator));
-
-            acceptCookiesButton.Click();
+            allLocationsOptionLocator = string.Format(allLocationsOptionLocator, option);
+            return GetLocationDropdownMenu().FindElement(By.CssSelector(allLocationsOptionLocator));
         }
 
-        public bool WaitDisplayed(WebDriverWait wait ,By locator)
+        private IWebElement GetSearchIcon() { return driver.FindElement(searchIconLocator); }
+        private IWebElement GetSearchTextBox() { return driver.FindElement(searchTextBoxLocator); }
+        private IWebElement GetMainFindButton() { return driver.FindElement(mainFindButtonLocator); }
+        private IWebElement GetFooter() { return driver.FindElement(footerLocator); }
+        private List<IWebElement> GetListElements() { return [.. driver.FindElements(listElementLocator)]; }
+
+        private void AcceptCookies()
         {
+            WaitCondition(() => GetAcceptCookiesButton().Enabled && GetAcceptCookiesButton().Displayed);
+            GetAcceptCookiesButton().Click();
+        }
+        private void WaitCondition(Func<bool> condition)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.IgnoreExceptionTypes(typeof(ElementClickInterceptedException));
             try
             {
-                return wait.Until(d => d.FindElement(locator).Displayed);
+                wait.Until(d => condition());
             }
-            catch (NoSuchElementException) { return false; }
-            catch (StaleElementReferenceException) { return false; }
+            catch (WebDriverTimeoutException) 
+            {
+                Console.WriteLine("Element was not found in time");
+            }
+            
         }
-        
+        private void ClickSearchIcon()
+        {
+            GetSearchIcon().Click();
+        }
+        private void InputSearchKeyword(string searchItem)
+        {
+            GetSearchTextBox().SendKeys(searchItem);
+        }
+        private void ClickMainFindButton()
+        {
+            GetMainFindButton().Click();
+        }
+        private void ClickCareerButton()
+        {
+            GetCareersTopMenu().Click();
+        }
+        private void InputProgrammingLanguage(string language)
+        {
+            GetLanguageTextBox().SendKeys(language);
+        }
+        private void ClickAllLocationsOption(string option)
+        {
+            MoveToLocationDropdownMenu(driver);
+            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
+            javaScriptExecutor.ExecuteScript("arguments[0].scrollIntoView(true);", GetAllLocationsOption(option));
+            GetAllLocationsOption(option).Click();
+        }
+        private void ClickRemoteCheckbox()
+        {
+            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
+            javaScriptExecutor.ExecuteScript("arguments[0].click();", GetRemoteCheckbox());
+        }
+        private void ClickFindButton()
+        {
+            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
+            javaScriptExecutor.ExecuteScript("arguments[0].click();", GetFindButton());
+        }
+        private void ClickViewAndApplyButton()
+        {
+            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)(driver);
+            javaScriptExecutor.ExecuteScript("arguments[0].scrollIntoView;", GetViewAndApplyButton());
+            GetViewAndApplyButton().Click();
+        }
+        private void ClickLocationDropdownButton(IWebDriver driver)
+        {
+            Actions action = new(driver);
+            action.MoveToElement(GetLocationDropdownButton());
+            GetLocationDropdownButton().Click();
+        }
+        private void MoveToLocationDropdownMenu(IWebDriver driver)
+        {
+            Actions action = new(driver);
+            action.MoveToElement(GetLocationDropdownMenu());
+        }
+        private void ScrollToFooter(IWebDriver driver)
+        {
+            Actions action = new(driver);
+            action.ScrollToElement(GetFooter()).Perform();
+        }
+        private List<IWebElement> FilterList(string keyword)
+        {
+            var filteredElements = GetListElements()
+                        .Where(element => element.GetAttribute("href")
+                        .Contains(keyword, StringComparison.OrdinalIgnoreCase))
+                        .ToList();
+            return filteredElements;
+        }
+
         [SetUp]
         public void Setup()
         {
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("--start-maximized");
             driver = new ChromeDriver(options);
-            driver.Navigate().GoToUrl("https://www.epam.com/");
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("AppSettings.json");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            var configuration = configBuilder.Build();
+            string baseUrl = configuration["BaseUrl"] ?? string.Empty;
+            driver.Navigate().GoToUrl(baseUrl);
 
         }
 
@@ -69,87 +168,42 @@ namespace epam_home_task
         [TestCase("C#", "All Locations")]
         [TestCase("Java", "All Locations")]
         [TestCase("Python", "All Locations")]
-        public void ValidateCriteriaBasedPositionSearch(string programmingLanguage, string location)
+        public void PageContainsInputProgrammingLanguage_DoesContainIt_Pass(string programmingLanguage, string location)
         {
-            Actions actions = new Actions(driver);
+            AcceptCookies();
+            ClickCareerButton();
+            InputProgrammingLanguage(programmingLanguage);
+            ClickLocationDropdownButton(driver);
+            ClickAllLocationsOption(location);
 
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            Assert.That(GetSelectedLocation().Text, Does.Contain(location));
 
-            AcceptCookies(wait);
-            
-            IWebElement careersTopMenu = driver.FindElement(By.XPath(careersTopMenuLocator));
-            careersTopMenu.Click();
-                        
-            IWebElement languageTextBox = driver.FindElement(By.Id(languageTextBoxLocator));
-            languageTextBox.SendKeys(programmingLanguage);
-
-            IWebElement locationDropdownButton = driver.FindElement(By.CssSelector(locationDropdownButtonLocator));
-            actions.MoveToElement(locationDropdownButton);
-            locationDropdownButton.Click();
-
-            IWebElement dropDownMenu = driver.FindElement(By.CssSelector(dropDownMenuLocator));
-            actions.MoveToElement(dropDownMenu).Perform();
-
-            allLocationsOptionLocator = string.Format(allLocationsOptionLocator, location);
-            IWebElement allLocationsOption = dropDownMenu.FindElement(By.CssSelector(allLocationsOptionLocator));
-            allLocationsOption.Click();
-
-            WaitDisplayed(wait, By.CssSelector(remoteCheckboxLocator));
-            IWebElement remoteCheckbox = driver.FindElement(By.CssSelector(remoteCheckboxLocator));
-            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
-            javaScriptExecutor.ExecuteScript("arguments[0].click();", remoteCheckbox);
-
-            IWebElement findButton = driver.FindElement(By.XPath(findButtonLocator));
-            findButton.Click();
-
-            IWebElement viewMoreButton = driver.FindElement(By.XPath(viewMoreButtonLocator));
-            actions.ScrollToElement(viewMoreButton);
-
-            WaitDisplayed(wait, By.CssSelector(viewAndApplyButtonLocator));
-            IWebElement viewAndApplyButton = driver.FindElement(By.CssSelector(viewAndApplyButtonLocator));
-            viewAndApplyButton.Click();
-
+            WaitCondition(() => GetRemoteCheckbox().Displayed);
+            ClickRemoteCheckbox();
+            ClickFindButton();
+            WaitCondition(() => GetViewAndApplyButton().Displayed);
+            ClickViewAndApplyButton();
             pageSource = driver.PageSource;
 
             Assert.That(pageSource, Does.Contain(programmingLanguage));
-            driver.Quit();
         }
 
         [Test]
         [TestCase("BLOCKCHAIN")]
         [TestCase("Cloud")]
         [TestCase("Automation")]
-        public void ValidateGlobalSearch(string keyword)
+        public void KeywordIsPresentInAllItemsOfList_IsNotPresentInAll_Fail(string keyword)
         {
-            Actions actions = new Actions(driver);
+            AcceptCookies();
+            ClickSearchIcon();
+            WaitCondition(() => GetSearchTextBox().Displayed);
+            InputSearchKeyword(keyword);
+            ClickMainFindButton();
+            ScrollToFooter(driver);
 
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-
-            AcceptCookies(wait);
-
-            IWebElement searchIcon = driver.FindElement(By.CssSelector(searchIconLocator));
-            searchIcon.Click();
-
-            WaitDisplayed(wait, By.Name(searchTextBoxLocator));
-            IWebElement searchTextBox = driver.FindElement(By.Name(searchTextBoxLocator));
-            searchTextBox.SendKeys(keyword);
-
-            IWebElement mainFindButton = driver.FindElement(By.XPath(mainFindButtonLocator));
-            mainFindButton.Click();
-
-            IWebElement footer = driver.FindElement(By.CssSelector(footerLocator));
-            actions.ScrollToElement(footer).Perform();
-
-            IList<IWebElement> listElements = driver.FindElements(By.CssSelector(listElementLocator));
-
-            var filteredElements = listElements
-                        .Where(element => element.GetAttribute("href").Contains(keyword, StringComparison.OrdinalIgnoreCase))
-                        .ToList();             
-            Console.WriteLine(listElements);
-            Assert.That(filteredElements, Is.EqualTo(listElements));
-
-
-            driver.Quit();
+            Assert.That(FilterList(keyword), Is.EqualTo(GetListElements())
+                ,$"Filtered list amount({FilterList(keyword).Count}) " +
+                $"is not the same as in Total list ({GetListElements().Count})");
         }
     }
 }
